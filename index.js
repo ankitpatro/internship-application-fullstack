71d375f63e08720d7d91009b691de7c460998eb5
Ankit Patro <37083691+ankitpatro@users.noreply.github.com>
2020-04-20 18:51:42 -0400

    Internship-assignment

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
+const URL_CLOUD = 'https://cfw-takehome.developers.workers.dev/api/variants'
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
+function handler(request) {
+    const init = {
+        headers: { 'content-type': 'application/json' },
+    }
+    const body = JSON.stringify({ some: 'json' })
+    return new Response(body, init)
+}
+class TitleHandler {
+    element(element) {
+        element.replace(
+            "<div align = 'center' <h1> Ankit's Internship Application</h1> </div>",
+            { html: true }
+        )
+    }
+}
+class DescriptionHandler {
+    element(element) {
+        element.replace('I Wonder what variant will I get next time...')
+    }
+}
+
+class UrlHandler {
+    element(element) {
+        element.setAttribute('href', 'https://github.com/ankitpatro')
+        element.setInnerContent('Check out my GitHub!')
+    }
+}
+
+var hwriter = new HTMLRewriter()
+hwriter
+
+    .on('title', new TitleHandler())
+    .on('p#description', new DescriptionHandler())
+    .on('a#url', new UrlHandler())
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+    try {
+        let response = await fetch(URL_CLOUD)
+        let rand = Math.floor(Math.random() * 2)
+        let data = await response.json()
+        const RANDOM_INDEX = Math.floor(Math.random() * data.variants.length) // generate random index in array
+        url = data.variants[RANDOM_INDEX]
+
+        let urlResponse = await fetch(url).catch(err =>
+            console.log(err.message)
+        )
+        let body = hwriter.transform(urlResponse)
+        console.log(body)
+        return body
+    } catch (e) {
+        console.log('Exception: ' + e)
+    }
 }
